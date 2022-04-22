46c4cb86ddb904bd7e9b3370233d7dbb5b04e046
jpg <jpg.soporte@gmail.com>
2022-04-21 20:01:25 -0400

    some plugin/block info update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ import save from './save';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( 'create-block/jpg-fnugg-block', {
+registerBlockType( 'jpg/fnugg-block', {
 	/**
 	 * @see ./edit.js
 	 */
